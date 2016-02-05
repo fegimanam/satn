5ef443d026555273104be41bfac0c574450d316d
fegimanam <ccaahh430@gmail.com>
2016-02-05 18:19:33 +0000

    Create nerkhadmin.lua

---
+++ plugins/nerkhadmin.lua
@@ -0,0 +1,20 @@
+Gmorr is back, [۰۵.۰۲.۱۶ ۲۱:۴۳]
+do
+
+function run(msg, matches)
+  return " For Promoting as an admin in Sultan : \n 15t Access : Only can banall Users \n 25t Access : Only Can Create and Manage Groups \n 35t Access : Can Access to Everything \n   baraye admin shodan dar bote Sultan : \n 15 t faghat mitavanad banall konad \n 25t  faghat mitavanad gp besazad va group ha ro manage konad \n 35t Dastresi kamel be hameye emkanat ra darad. \n baraye admin shodan be @alifengi moraje'e konid.\n For promoting to admin contact to @alifengi "
+  end
+return {
+  description = " nerkhadmin ",
+  usage = "nerkhadmin",
+  patterns = {
+    "^Admin$",
+    "^admin$",
+    "^adminnerkh$",
+    "^Adminnerkh$",
+    "^Nerkhadmin$",
+    "^nerkhadmin$"
+  },
+  run = run
+}
+end
